3c487e5e3b7cbdded4e60b0f2d7e31ac74341c29
mehrain <marijnrovers@live.nl>
2024-08-21 00:46:30 +0200

    bedtime

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import pygame 
 from constants import *
 
-GAMERUNNING = True
-
-
-
 def main ():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    while GAMERUNNING is True:
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    while GAME_RUNNING is True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill((0, 0, 0))
         pygame.display.flip
     
